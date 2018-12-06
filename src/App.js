64b34b7425ca79e6031f2baa26b098678e17dea9
Ming Ho <hkboi108@hotmail.com>
2018-12-06 10:43:54 -0800

    Add Browser Router base name

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends React.Component {
       <div className="app">
         <div>
           <Route path='/' component={ Main } />
-          <Route path='reactnd-project-myreads/search' component={ Search } />
+          <Route path='/search' component={ Search } />
         </div>
       </div>
     )
